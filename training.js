cb3218c5009d1c47ad5917fcb9bb3918d1648b2a
DovitosMan <kashapov_dmitry@bk.ru>
2023-11-09 23:23:03 +0500

    начинаем js

--- training.js
+++ training.js
@@ -27,28 +27,26 @@ if ((Text_2.length>Text_1.length) && (Text_2.length>Text_3.length)) {
 if ((Text_3.length>Text_1.length) && (Text_3.length>Text_2.length)) {
     console.log("Вернуть самую длинную строку - ",Text_3)
 } 
-//Является ли слова палиндромом?
-let n1 = Text_1.length-1;
-for ( let i = 0; i < n1/2; i++)  {
-    if (Text_1[i]==Text_1[n1-i]){
-        console.log(Text_1," - слово палиндром");
-    } else 
-    console.log(Text_1," - слово не палиндром");
+//Является ли слово палиндромом?
+function palindrom(Text_n){
+    let n = Text_n.length-1;
+    let palin = 0;
+    for ( let i = 0; i < n/2; i++)  {
+        if (Text_n[i]==Text_n[n-i]) {
+            palin = 1;
+        }
+        else {
+            palin = 0;
+            break;
+        }
+    }
+    if (palin)
+    console.log(Text_n," - слово палиндром"); else
+    console.log(Text_n," - слово не палиндром");
 }
-let n2 = Text_2.length-1;
-for ( let i = 0; i < n2/2; i++)  {
-    if (Text_2[i]==Text_2[n2-i]){
-        console.log(Text_2," - слово палиндром");
-    } else 
-    console.log(Text_2," - слово не палиндром");
-}
-let n3 = Text_3.length-1;
-for ( let i = 0; i < n3/2; i++)  {
-    if (Text_3[i]==Text_3[n3-i]){
-        console.log(Text_3," - слово палиндром");
-    } else 
-    console.log(Text_3," - слово не палиндром");
-}
+palindrom(Text_1);
+palindrom(Text_2);
+palindrom(Text_3);
 //сумма элементов массива
 let massiv = [5,2,1,3,4];
 let result = massiv.reduce((sum, current) => sum + current, 0);
@@ -66,14 +64,19 @@ for (let j = massiv.length - 1; j > 0; j--) {
   console.log("Отфильтровать массив чисел -",massiv);
 //массив объектов 
 let object = [{ name: "Bob", age: 50}, { name: "Jane", age: 64}, { name: "Jack", age: 25}];
-
-let find = object.find(index => index.age == 50);  
-object.splice(find,1);
-console.log("Отфильтровать массив объектов - ",object);
+let done = object.filter(index => index.age <= 50);
+console.log("Отфильтровать массив объектов - ",done);
 //склонировать объект
 let object_2 = { name: "Bob", age: 50, children: [ { name: "Marie", age: 16}, { name: "Jame", age: 12} ] };
 let clone = {}
+let child = object_2.children;
 for (let key in object_2) {
+    if (key == 'children') 
+        clone.children=object_2.children.slice();
+    else
     clone[key] = object_2[key];
 }
 console.log("Склонировать объект  - ",clone);
+// const clone3 = structuredClone(object_2);
+// const clone2 = JSON.parse('obj-from-backend');
+import (api-response.json)
---
+++ weather.js
@@ -0,0 +1,196 @@
+const locations = [
+    {
+      "id": 1,
+      "name": "Biarritz",
+      "country": "FR",
+      "days": [
+        {
+          "id": 1,
+          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 0,
+          "humidity": 42,
+          "wind": 3
+        },
+        {
+          "id": 2,
+          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "cloudy",
+          "temperature": 24,
+          "precipitation": 5,
+          "humidity": 64,
+          "wind": 4
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "rainy",
+          "temperature": 18,
+          "precipitation": 10,
+          "humidity": 80,
+          "wind": 5
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 4,
+          "humidity": 60,
+          "wind": 4
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Moscow",
+      "country": "RU",
+      "days": [
+        {
+          "id": 1,
+          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 0,
+          "humidity": 42,
+          "wind": 3
+        },
+        {
+          "id": 2,
+          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "cloudy",
+          "temperature": 24,
+          "precipitation": 5,
+          "humidity": 64,
+          "wind": 4
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "rainy",
+          "temperature": 18,
+          "precipitation": 10,
+          "humidity": 80,
+          "wind": 5
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 4,
+          "humidity": 60,
+          "wind": 4
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "London",
+      "country": "UK",
+      "days": [
+        {
+          "id": 1,
+          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 0,
+          "humidity": 42,
+          "wind": 3
+        },
+        {
+          "id": 2,
+          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "cloudy",
+          "temperature": 24,
+          "precipitation": 5,
+          "humidity": 64,
+          "wind": 4
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "rainy",
+          "temperature": 18,
+          "precipitation": 10,
+          "humidity": 80,
+          "wind": 5
+        },
+        {
+          "id": 1,
+          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+          "weather": "sunny",
+          "temperature": 29,
+          "precipitation": 4,
+          "humidity": 60,
+          "wind": 4
+        }
+      ]
+    }
+  ];
+
+//   console.log(locations);
+
+window.addEventListener("load", function(){
+    const currentLocation = locations[1];
+    const { days } = currentLocation;
+    
+    const cityElement = document.getElementById("city");
+    const countryElement = document.getElementById("country");
+
+    const weekDaysContainer = document.querySelector('.card-right__week-day');
+
+    console.log(weekDaysContainer);
+
+    days.forEach((day) => {
+        const { temperature, date } = day;
+        const dateFormat = new Date(date);
+
+        const weekDay = document.createElement('div');
+        weekDay.classList.add('card-right__week-day-text');
+        weekDay.innerText = dateFormat.getDay();
+
+        const weekTemperature = document.createElement('div');
+        weekTemperature.classList.add('card-right__week-day-temperature');
+        weekTemperature.innerText = temperature;
+
+        const dayElement = document.createElement('div');
+        dayElement.classList.add('card-right__week-day--unactive');
+
+        dayElement.append(weekDay);
+        dayElement.append(weekDaysContainer);
+
+        weekDaysContainer.append(dayElement);
+    });
+
+   
+
+    // const widgets = document.querySelectorAll('div');
+    // console.log(widgets);
+
+    // console.log(cityElement);
+    if (cityElement) {
+        cityElement.innerText = currentLocation.name;
+    }
+});
+
+// fn1();
+
+
+// function fn1 (arg1, arg2){
+//     console.log('1');
+// }
+// const fn2 = function(arg1, arg2) {
+//     console.log('2');
+// }
+// const fn3 = (arg1, arg2) => {
+//     console.log('3');
+// }
+
+// const fn4 = fn1;
+
+
+// fn2();
+// fn3();
+// fn4();
