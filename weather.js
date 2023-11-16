5ac33223ff93ad381c05d4ce868aa6b58cf0238c
DovitosMan <kashapov_dmitry@bk.ru>
2023-11-16 21:34:35 +0500

    поделено все на функции и динамичное изменение

--- weather.js
+++ weather.js
@@ -1,139 +1,188 @@
 const locations = [
-    {
-      "id": 1,
-      "name": "Biarritz",
-      "country": "FR",
-      "days": [
-        {
-          "id": 1,
-          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 0,
-          "humidity": 42,
-          "wind": 3
-        },
-        {
-          "id": 2,
-          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "cloudy",
-          "temperature": 24,
-          "precipitation": 5,
-          "humidity": 64,
-          "wind": 4
-        },
-        {
-          "id": 3,
-          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "rainy",
-          "temperature": 18,
-          "precipitation": 10,
-          "humidity": 80,
-          "wind": 5
-        },
-        {
-          "id": 4,
-          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 4,
-          "humidity": 60,
-          "wind": 4
-        }
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Moscow",
-      "country": "RU",
-      "days": [
-        {
-          "id": 1,
-          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 0,
-          "humidity": 42,
-          "wind": 3
-        },
-        {
-          "id": 2,
-          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "cloudy",
-          "temperature": 24,
-          "precipitation": 5,
-          "humidity": 64,
-          "wind": 4
-        },
-        {
-          "id": 3,
-          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "rainy",
-          "temperature": 18,
-          "precipitation": 10,
-          "humidity": 80,
-          "wind": 5
-        },
-        {
-          "id": 4,
-          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 4,
-          "humidity": 60,
-          "wind": 4
-        }
-      ]
-    },
-    {
-      "id": 3,
-      "name": "London",
-      "country": "UK",
-      "days": [
-        {
-          "id": 1,
-          "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 0,
-          "humidity": 42,
-          "wind": 3
-        },
-        {
-          "id": 2,
-          "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "cloudy",
-          "temperature": 24,
-          "precipitation": 5,
-          "humidity": 64,
-          "wind": 4
-        },
-        {
-          "id": 3,
-          "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "rainy",
-          "temperature": 18,
-          "precipitation": 10,
-          "humidity": 80,
-          "wind": 5
-        },
-        {
-          "id": 4,
-          "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
-          "weather": "sunny",
-          "temperature": 29,
-          "precipitation": 4,
-          "humidity": 60,
-          "wind": 4
-        }
-      ]
-    }
-  ];
-
-window.addEventListener("load", function(){
-    const daysNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  {
+    "id": 1,
+    "name": "Biarritz",
+    "country": "FR",
+    "days": [
+      {
+        "id": 1,
+        "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 29,
+        "precipitation": 0,
+        "humidity": 42,
+        "wind": 3
+      },
+      {
+        "id": 2,
+        "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "cloudy",
+        "temperature": 24,
+        "precipitation": 5,
+        "humidity": 64,
+        "wind": 4
+      },
+      {
+        "id": 3,
+        "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "rainy",
+        "temperature": 18,
+        "precipitation": 10,
+        "humidity": 80,
+        "wind": 5
+      },
+      {
+        "id": 4,
+        "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 29,
+        "precipitation": 4,
+        "humidity": 60,
+        "wind": 4
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Moscow",
+    "country": "RU",
+    "days": [
+      {
+        "id": 1,
+        "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 20,
+        "precipitation": 3,
+        "humidity": 53,
+        "wind": 4
+      },
+      {
+        "id": 2,
+        "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "cloudy",
+        "temperature": 24,
+        "precipitation": 5,
+        "humidity": 64,
+        "wind": 4
+      },
+      {
+        "id": 3,
+        "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "rainy",
+        "temperature": 18,
+        "precipitation": 10,
+        "humidity": 80,
+        "wind": 5
+      },
+      {
+        "id": 4,
+        "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 29,
+        "precipitation": 4,
+        "humidity": 60,
+        "wind": 4
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "name": "London",
+    "country": "UK",
+    "days": [
+      {
+        "id": 1,
+        "date": "Thu Nov 09 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 29,
+        "precipitation": 0,
+        "humidity": 42,
+        "wind": 3
+      },
+      {
+        "id": 2,
+        "date": "Thu Nov 10 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "cloudy",
+        "temperature": 24,
+        "precipitation": 5,
+        "humidity": 64,
+        "wind": 4
+      },
+      {
+        "id": 3,
+        "date": "Thu Nov 11 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "rainy",
+        "temperature": 18,
+        "precipitation": 10,
+        "humidity": 80,
+        "wind": 5
+      },
+      {
+        "id": 4,
+        "date": "Thu Nov 12 2023 18:21:45 GMT+0500 (Yekaterinburg Standard Time)",
+        "weather": "sunny",
+        "temperature": 29,
+        "precipitation": 4,
+        "humidity": 60,
+        "wind": 4
+      }
+    ]
+  }
+];
+const log = console.log
+const App = {
+  setup() {
     const [currentLocation] = locations;
-    const { name, country, days } = currentLocation;
+
+    this.showLocation(currentLocation);
+    // const [currentDay] = days;
+    // this.showDay(currentDay);
+    this.modal();
+    this.selectListener();
+  },
+  modal() {
+    const modalElement = document.getElementById('pop-up');
+    if (modalElement) {
+      document.querySelectorAll('.open-modal').forEach(button => {
+        button.addEventListener('click', () => {
+          modalElement.classList.remove('closed');
+          
+        })
+      });
+    }
+    if (modalElement) {
+      document.querySelectorAll('.close-modal').forEach(button => {
+        button.addEventListener('click', () => {
+          modalElement.classList.add('closed');
+        })
+      });
+    }
+  },
+  selectListener() {
+    //City Select
+
+    const citySelectElement = document.getElementById('location-select');
+    citySelectElement.onchange = () => {
+      const newCity = locations.find(location => location.id == citySelectElement.value);
+      // console.log(this);
+      this.showLocation(newCity);
+    };
+    this.createSelectOptions(citySelectElement);
+  },
+  createSelectOptions(citySelectElement) {
+    locations.forEach(location => {
+      const option = document.createElement('option');
+      option.innerText = `${location.name}, ${location.country}`;
+      option.value = location.id;
+      option.id = location.id
+      citySelectElement.appendChild(option);
+    });
+  },
+  showLocation(location) {
+    // debugger;
+    const { name, country, days } = location;
+    const [firstDay] = days;
+    const daysNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const dateElement = document.getElementById('date');
     const cityElement = document.getElementById('city');
     const countryElement = document.getElementById('country');
@@ -143,60 +192,48 @@ window.addEventListener("load", function(){
     const weekDay = document.getElementById('current-day');
     const weekTemperature = document.getElementById('current-temperature');
     const weekDaysContainer = document.querySelector('.card-right__widget');
+    weekDaysContainer.innerHTML = '';
+
     days.forEach(day => {
-      const {temperature, date} = day;
+      const { temperature, date } = day;
+      // const [weather, temperature, percipitation, humidity, wind, date] = day;
       const dateFormat = new Date(date);
-      if (day.id = 1){
-        const dayElement = createDivElement('card-right__widget--active');
-      } else {const dayElement = createDivElement('card-right__widget--unactive');}
+      const dayElement = createDivElement('card-right__widget--unactive');
       const weekDayEl = createDivElement('card-right__widget-text', daysNames[dateFormat.getDay()])
       const tempDayEl = createDivElement('card-right__widget-temperature', temperature + " °C");
       dayElement.append(weekDayEl);
       dayElement.append(tempDayEl);
       weekDaysContainer.append(dayElement);
       if (weekDay) {
-          weekDay.innerText = daysNames[dateFormat.getDay()];
+        weekDay.innerText = daysNames[dateFormat.getDay()];
       };
       if (dateElement) {
-          dateElement.innerText = daysNames[dateFormat.getDay()];
+        dateElement.innerText = daysNames[dateFormat.getDay()];
       }
       if (weekTemperature) {
         weekTemperature.innerText = temperature + "°C";
       };
       if (percipitationElement) {
-        percipitationElement.innerText = days[0].precipitation + "%";
-        console.log (days);
+        percipitationElement.innerText = firstDay.precipitation + "%";
       };
       if (humidityElement) {
-          humidityElement.innerText = days[0].humidity + "%";
+        humidityElement.innerText = firstDay.humidity + "%";
       };
       if (windElement) {
-          windElement.innerText = days[0].wind + "km/h";
+        windElement.innerText = firstDay.wind + "km/h";
       };
-  });    
-    if (cityElement) {
+      if (cityElement) {
         cityElement.innerText = name + ", ";
-    };
-    if (countryElement) {
+      };
+      if (countryElement) {
         countryElement.innerText = country;
-    };
-    
-    const modalElement = this.document.getElementById('pop-up'); 
+      };
+    });
+  }
+};
 
-    if (modalElement) {
-      document.querySelectorAll('.open-modal').forEach(button => {
-        button.addEventListener('click', () => {
-          modalElement.classList.remove('closed');
-        })
-      });
-    }
-    if (modalElement){
-      document.querySelectorAll('.close-modal').forEach(button => {
-        button.addEventListener('click',()=>{
-          modalElement.classList.add('closed');
-        })
-      })
-    }
+window.addEventListener("load", function () {
+  App.setup();
 });
 
 
